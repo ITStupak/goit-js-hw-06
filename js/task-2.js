b06cd3ab5e44bbdaff795a1fac511eb6b3a382c4
Sergey Stupak <itstupak@gmail.com>
2024-05-01 03:08:09 +0200

    created structure hw-6

---
+++ js/task-2.js
@@ -0,0 +1,33 @@
+"use strict";
+
+class Storage {
+  #items;
+  constructor(params) {
+    this.#items = params;
+  }
+  getItems() {
+    return this.#items;
+  }
+  addItem(newItem) {
+    return this.#items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    if (this.#items.includes(itemToRemove)) {
+      const itemToRemoveIndex = this.#items.indexOf(itemToRemove);
+      this.#items.splice(itemToRemoveIndex, 1);
+    }
+    return this.#items;
+  }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
